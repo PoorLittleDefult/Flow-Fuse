163d090aec0d062ca815276ad2f9eacd032461bc
ForTheWinAlways <82647017+PoorLittleDefult@users.noreply.github.com>
2023-03-31 10:22:51 +1300

    basic template made

--- app/routes.py
+++ app/routes.py
@@ -3,4 +3,8 @@ from flask import render_template, redirect
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    return render_template('index.html')
+    return render_template('home.html')
+
+@app.route('/product', methods=['GET', 'POST'])
+def product():
+    return render_template('product.html')
