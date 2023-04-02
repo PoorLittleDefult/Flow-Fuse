4c431858387c2b28dcbdab27b63d674d153fa1b0
ForTheWinAlways <82647017+PoorLittleDefult@users.noreply.github.com>
2023-04-02 15:14:18 +1200

    can add items to database + new form
    Items are able to be added to the database and then displayed on the products page

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from app import app, db
-from flask import render_template, redirect
+from app import models
+from flask import render_template, redirect, request, url_for
 from app.models import Item
 
 @app.route('/', methods=['GET', 'POST'])
@@ -10,3 +11,16 @@ def home():
 def product():
     items = Item.query.all()
     return render_template('product.html', items=items)
+
+
+@app.route('/new-item', methods=['GET', 'POST'])
+def new_item():
+    if request.method == 'POST':
+        item_name = request.form['item_name']
+        category = request.form['category']
+        price = request.form['price']
+        item = Item(item_name=item_name, category=category, price=price)
+        db.session.add(item)
+        db.session.commit()
+        return redirect(url_for('home'))
+    return render_template('new_item.html')
